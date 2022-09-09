e2884cc29e5ddea26f46d0254348fb12dc88eae1
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-09 08:54:03 -0500

    Implement ship placement

--- src/factories/GameBoard.js
+++ src/factories/GameBoard.js
@@ -1,5 +1,25 @@
-class GameBoard {
+const GAME_BOARD_WIDTH = 10;
+const GAME_BOARD_HEIGHT = 10;
 
+class GameBoard {
+  width;
+  height;
+  ships;
+  shipCount;
+
+  constructor() {
+    this.width = GAME_BOARD_WIDTH;
+    this.height = GAME_BOARD_HEIGHT;
+    this.ships = [];
+    this.shipCount = 0;
+  }
+
+  place = (ship, x, y, vertical = false) => {
+    if (vertical ? ship.length + y > this.height : ship.width + x > this.width)
+      throw new Error("Ship can not be placed out of bounds.");
+    this.ships.push({ ship, x, y });
+    this.shipCount += 1;
+  };
 }
 
 module.exports = GameBoard;
