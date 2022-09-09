211cf5edd88243e92815335a1c11a96e65c118ab
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-09 06:48:09 -0500

    Pass hit test

--- src/factories/Ship.ts
+++ src/factories/Ship.ts
@@ -1,8 +1,15 @@
 class Ship {
   length;
+  hits;
   constructor(length) {
     this.length = length;
+    this.hits = 0;
   }
+
+  hit = (position) => {
+    if (position < this.length && position >= 0) this.hits |= 1 << position;
+    else throw new Error('Hit position out of bounds');
+  };
 }
 
 module.exports = Ship;
