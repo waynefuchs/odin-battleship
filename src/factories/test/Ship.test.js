b08869bbb84dbc50d226d7d22f3bb15d131a47c7
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-09 07:02:29 -0500

    Add ship sink test

--- src/factories/test/Ship.test.js
+++ src/factories/test/Ship.test.js
@@ -26,3 +26,13 @@ test('Ship hits out of bounds throw an error', () => {
   expect(() => ship.hit(2)).not.toThrow(Error);
   expect(() => ship.hit(3)).toThrow(Error);
 })
+
+test('Ship can be sunk', () => {
+  expect(ship.isSunk()).toBe(false);
+  ship.hit(0);
+  expect(ship.isSunk()).toBe(false);
+  ship.hit(1);
+  expect(ship.isSunk()).toBe(false);
+  ship.hit(2);
+  expect(ship.isSunk()).toBe(true);
+})
