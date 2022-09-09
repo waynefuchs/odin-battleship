6fb6200983e15e3bc7e62c225d59e8ce7cb71022
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-09 08:42:13 -0500

    Add ship placement test

--- src/factories/test/GameBoard.test.js
+++ src/factories/test/GameBoard.test.js
@@ -1,4 +1,5 @@
 const GameBoard = require('../GameBoard');
+const Ship = require('../Ship');
 
 let board;
 
@@ -8,4 +9,11 @@ beforeEach(() => {
 
 test('Board class exists', () => {
   expect(board).not.toBeNull();
+})
+
+test('Can place ship on board', () => {
+  const ship = new Ship(3);
+  expect(board.shipCount).toBe(0);
+  board.place(ship, 0, 0);
+  expect(board.shipCount).toBe(1);
 })
