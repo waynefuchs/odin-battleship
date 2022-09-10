03e93d9b4f681aafd23944624ccedfcf1b44446b
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-10 00:13:47 -0500

    Initialize board in constructor

--- src/factories/Player.js
+++ src/factories/Player.js
@@ -1,7 +1,11 @@
+const GameBoard = require("./GameBoard");
+
 class Player {
+  board;
   isAi;
 
-  constructor(ai = false) {
+  constructor(board = null, ai = false) {
+    this.board = board ?? new GameBoard();
     this.isAi = ai;
   }
 }
