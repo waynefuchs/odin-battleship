7737517eff4b8946785f69802025c1669b651d24
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-11 05:54:49 -0500

    Implement placing unplaced ship method

--- src/factories/Human.js
+++ src/factories/Human.js
@@ -4,16 +4,28 @@ const Ship = require("./Ship");
 class Human extends Player {
   constructor() {
     super();
-    this.unplacedShips.push(new Ship(5, 'Carrier'));
-    this.unplacedShips.push(new Ship(4, 'Battleship'));
-    this.unplacedShips.push(new Ship(3, 'Cruiser'));
-    this.unplacedShips.push(new Ship(3, 'Submarine'));
-    this.unplacedShips.push(new Ship(2, 'Destroyer'));
+    this.unplacedShips.push(new Ship(5, "Carrier"));
+    this.unplacedShips.push(new Ship(4, "Battleship"));
+    this.unplacedShips.push(new Ship(3, "Cruiser"));
+    this.unplacedShips.push(new Ship(3, "Submarine"));
+    this.unplacedShips.push(new Ship(2, "Destroyer"));
 
-    const ship = new Ship(10, 'Megaship');
-    this.board.place(ship, 0, 0);
-    this.name = 'Human Player';
-    this.isReady = true;
+    this.name = "Human Player";
+  }
+
+  placeNextUnplacedShip(x, y, vertical) {
+    if (this.unplacedShips.length <= 0)
+      throw new Error("There are no unplaced ships to place");
+
+    try {
+      this.board.place(this.unplacedShips.at(0), x, y, vertical);
+      this.unplacedShips.shift();
+      if(this.unplacedShips.length <= 0) this.isReady = true;
+    } catch (error) {
+      return false;
+    }
+
+    return true;
   }
 }
 
